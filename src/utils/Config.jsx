c9485e6931320a8fe1daeb573f112fd07d687381
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-04-08 11:49:00 +0530

    added server url to clinet

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api/v1"
+// export const BASE_URL = "http://localhost:8080/api/v1"
+export const BASE_URL = "https://blog-application-66t5.onrender.com/api/v1"
